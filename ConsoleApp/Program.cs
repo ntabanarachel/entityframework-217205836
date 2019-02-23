cc6355e6e5e8d76ec260484d87f495faacc4830e
rachel <ntabanarachel@gmail.com>
2019-02-23 18:40:09 +0200

    initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MovieContext())
+            {
+                Console.WriteLine("*************************WELCOME TO JORAH COMPANY***************************");
+                Console.WriteLine("enter Movie title");
+                string title = Console.ReadLine();
+                Console.WriteLine("enter Movie release year");
+                string year = Console.ReadLine();
+                Console.WriteLine("enter Movie category");
+                string categories = Console.ReadLine();
+                db.Moviess.Add(new Movietbl { title = title });
+                db.Moviess.Add(new Movietbl { year = year });
+                db.Moviess.Add(new Movietbl { category_name = categories });
+                var count = db.SaveChanges();
+                Console.WriteLine("-{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("title:" + "Year" + "Category");
+                Console.WriteLine("*************************enjoy movies*******************************");
+                foreach (var Movietbl in db.Moviess)
+                {
+
+                    Console.Write("{0}, {1}, {2}", Movietbl.title, Movietbl.year, Movietbl.category_name, "");
+                    Console.WriteLine("");
+
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApp/model.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp
+{
+    public class MovieContext : DbContext
+    {
+        public DbSet<Movietbl> Moviess { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFGetStartted.MoviessApp.NewDb;Trusted_Connection=True;");
+        }
+    }
+
+    public class Movietbl
+    {  
+        [Key]
+        public int MovieId { get; set; }
+        public string title { get; set; }
+        public string year { get; set; }
+        public string category_name { get; set; }
+    }
+
+
+}
